0ce7650c93b6615c48de5b46de99cdd5f8cb5345
Sudesh Arunachalam <Sudesh2746@gmail.com>
2025-03-11 18:41:36 +1100

    add clin not int error

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -41,11 +41,12 @@ def main():
 
         go = st.button("Start converting")
 
-        try:
-            clint = int(clin)
-        except TypeError:
-            clin = None
-            st.error("Make sure your clin group is a number.")
+        if clin:
+            try:
+                clint = int(clin)
+            except ValueError:
+                clin = None
+                st.error("Make sure your clin group is a number.")
 
         if pbl and clin and campus and go:
             saved = process_xlsx(pbl.upper(), clin, campus, ws)
