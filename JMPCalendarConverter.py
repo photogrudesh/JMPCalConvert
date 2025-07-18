03f595d70c81ebc71bf4c182c69cc378725fe802
Sudesh Arunachalam <Sudesh2746@gmail.com>
2025-07-18 13:22:37 +1000

    Updated timetable

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -15,8 +15,8 @@ def main():
     st.divider()
     st.subheader("Upload xlsx")
 
-    use_latest = st.checkbox("Use latest year 1 timetable (last updated: 09:31 06/06/2025)", value=True)
-    file = "MEDI1101A 06062025.xlsx"
+    use_latest = st.checkbox("Use latest year 1 timetable (last updated: 13:17 18/07/2025)", value=True)
+    file = "MEDI1101B 18072025.xlsx"
 
     date_start = datetime.date(1970, 1, 1)
     date_end = datetime.date(3000, 1, 1)
