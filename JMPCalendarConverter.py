4686bf1225272a15f4c368a55ff776f0a3b09a00
Sudesh Arunachalam <Sudesh2746@gmail.com>
2025-07-21 15:08:38 +1000

    Updated timetable

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -88,6 +88,8 @@ def main():
                 st.error(f"Central Coast does not have clinical group {clin}")
                 valid_selection = False
 
+        go = False
+
         col11, col12 = st.columns(2)
 
         if valid_selection:
