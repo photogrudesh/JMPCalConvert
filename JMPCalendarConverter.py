24eff18b9b19a863af55e222399ad4f9efe05369
photogrudesh <sudesh2746@gmail.com>
2026-02-11 10:44:35 +1100

    fix zoom hyperlinking

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -290,7 +290,11 @@ def process_xlsx(pbl, clin, comm, uni_format, campus, ws):
 
         if "Zoom" in str(i[cal_index(uni_format, "venue")]):
             try:
-                i[cal_index(uni_format, "venue")] = ws.cell(row=row_num, column=8).hyperlink.target
+                if uni_format == "University of Newcastle":
+                    zoom_col = 8
+                elif uni_format == "University of New England":
+                    zoom_col = 7
+                i[cal_index(uni_format, "venue")] = ws.cell(row=row_num, column=zoom_col).hyperlink.target
             except AttributeError:
                 pass
 
