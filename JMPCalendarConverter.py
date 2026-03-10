f6ce8ffe022b8d6f622601a74ea999071a14b065
photogrudesh <sudesh2746@gmail.com>
2026-03-11 09:05:14 +1100

    add spreadsheet download button

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -94,7 +94,7 @@ def main_ui():
 
     uni_format = format_selection(university)
 
-    file_select, file_dl = st.columns([3, 1])
+    file_select, file_dl = st.columns([5, 2])
 
     with file_select:
         if uni_format:
