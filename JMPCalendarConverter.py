45bc500aed6ffa8a1dab3d734d3eaa1ae88c512c
photogrudesh <sudesh2746@gmail.com>
2026-05-25 20:26:28 +1000

    add browser cookies

--- JMPCalendarConverter.py
+++ JMPCalendarConverter.py
@@ -143,13 +143,13 @@ def main_ui():
             pbl = controller.get('pbl')
             clin = controller.get('clin')
             comm = controller.get('comm')
-            print("hi")
+            st.write("hi")
         except AttributeError:
             campus = None
             pbl = None
             clin = None
             comm = None
-            print("hi :(")
+            st.write("hi :(")
 
 
         column1, column2 = st.columns(2)
